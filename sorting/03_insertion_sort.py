c98e03b0aacf75312c42170d925344c354f7a5dc
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-07-03 07:42:59 -0400

    03_insertion_sort updating with shift based approach

--- sorting/03_insertion_sort.py
+++ sorting/03_insertion_sort.py
@@ -20,7 +20,7 @@ Explanation: After sorting the array is: 1,2,3,4,5
 Solution Approach:
 
 1. Using the swapping concept, start from the first postition element and check it with the zeroth position, swap it if needed else break out of the loop. This continues till all the elements are sorted.
-
+2. Using the shifting approach, which is more efficient than swapping.
 
 
 '''
@@ -35,3 +35,19 @@ def insertion_sort(arr):
     return arr
 
 print(insertion_sort([13, 46, 24, 52, 20, 9]))
+
+
+def insertion_shift(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+print(insertion_shift([13, 46, 24, 52, 20, 9]))
