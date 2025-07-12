b19bc8e81278a7ba702b71bcfe1f2b863fb4445c
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-07-11 20:20:45 -0400

    06_left_rotate_by_k updated with several reverse methods and more efficient left rotate

--- arrays/easy/06_left_rotate_by_k.py
+++ arrays/easy/06_left_rotate_by_k.py
@@ -27,3 +27,47 @@ def left_rotate_k(nums, k):
     return nums[-k:] + nums[:-k]
 
 print(left_rotate_k([1,2,3,4,5], 3))
+
+
+def reverse(arr):
+    # reverse_arr = []
+    # for i in range(len(arr) - 1, -1, -1):
+    #     reverse_arr.append(arr[i])
+    #
+    # return reverse_arr
+
+    # reverse_arr = list( reversed(arr))
+    # return reverse_arr
+
+    return arr[::-1]
+
+    # left, right = 0, len(arr) - 1
+    #
+    # while left < right:
+    #     arr[left], arr[right] = arr[right], arr[left]
+    #     left += 1
+    #     right -= 1
+    #
+    # return arr
+
+# print(reverse([1,2,3,4,5]))
+
+def reverse_two(arr, left, right):
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+    return arr
+
+def rotate_reverse(arr, k):
+    k = k % len(arr)
+
+    reverse_two(arr, 0, len(arr) - 1)
+    reverse_two(arr, 0, k - 1)
+    reverse_two(arr, k, len(arr) - 1)
+
+
+    return arr
+
+print(rotate_reverse([1,2,3,4,5], 3))
