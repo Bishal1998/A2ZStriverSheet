268c8075d76286781f523cf4201558e8ae31ae05
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-07-17 08:07:21 -0400

    13_longest_subarray_positives updated with the optimized solution

--- arrays/easy/13_longest_subarray_positives.py
+++ arrays/easy/13_longest_subarray_positives.py
@@ -14,7 +14,10 @@ Result: 3
 Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
 
 Solution Approach:
-1. Using triple nested loops to generate all the sub-array and checked whether the sum of the sub array is equal to the given sum K, if equals save the maximum length of the sub-array.
+1. Using triple nested loops to generate all the sub-array and checked whether the sum of the sub array is equal to the given sum K, if equals save the maximum length of the sub-array. O(n3)
+2. Using nested loops and this will reduce the complexity to O(n^2)
+
+
 
 
 
@@ -39,3 +42,44 @@ def brute_longest_subarray(arr, K):
 
 
 print(brute_longest_subarray([2,3,5,1,9], 10))
+
+
+def longest_subarray(arr, K):
+    length = 0
+
+    for i in range(len(arr)):
+        s = 0
+        for j in range(i, len(arr)):
+            s += arr[j]
+
+            if s == K:
+                length = max(length, j - i + 1)
+
+    return length
+
+print(longest_subarray([2,3,5,1,9], 10))
+
+
+def longest_subarray_sum(arr, K):
+
+    length = 0
+    prefix_sum = 0
+    sum_indices = {}
+
+    for i in range(0, len(arr)):
+        prefix_sum += arr[i]
+
+        if prefix_sum == K:
+            length = i + 1
+
+        if prefix_sum - K in sum_indices:
+            length = max(length, i - sum_indices[prefix_sum - K])
+
+        if prefix_sum not in sum_indices:
+            sum_indices[prefix_sum] = i
+
+    return length
+
+print(longest_subarray_sum([10, -10, 20, 30], 5))
+
+
