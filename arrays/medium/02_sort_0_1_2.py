7a29834f89f3b231c6a9142683b71501090a8338
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-07-17 20:00:21 -0400

    02_sort_0_1_2 completed

---
+++ arrays/medium/02_sort_0_1_2.py
@@ -0,0 +1,38 @@
+'''
+Problem Statement: Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Input: nums = [0]
+Output: [0]
+
+Solution: We can count 0,1 and 2 and run the loop to generate new_arr but it's not efficient.
+So, for that we need to traverse in one pass without any extra space.
+'''
+
+
+def sort_012(nums):
+
+    low = mid = 0
+    high = len(nums) - 1
+
+    while mid <= high:
+
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
+    return nums
+
+print(sort_012([2,0,2,1,1,0]))
